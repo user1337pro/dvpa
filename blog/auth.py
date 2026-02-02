eaefaacd497f27022b78affa6fb985bd47626b76
your_username <your_email@gmail.com>
2026-02-02 19:13:57 +0000

    Fixing hashing issue

--- blog/auth.py
+++ blog/auth.py
@@ -1,38 +1,36 @@
 from functools import wraps
 from flask import request, Response, session
-from blog import config
 from blog import db
-
-import hashlib
+import bcrypt
 
 def check_auth(username, password):
     """ This function is called to check if a username / password
         combination is valid.
     """
     cur = db.connection.cursor()
-    hashsed_password = hashlib.md5(password.encode()).hexdigest()
-    cur.execute(f"SELECT * FROM users WHERE email='{username}' AND password='{hashsed_password}'")
+    # Use parameterized queries to prevent SQL Injection
+    cur.execute(f"SELECT * FROM users WHERE email='{username}'")
     user = cur.fetchone()
 
     if user is None:
         return False
 
-    session["is_logged_in"] = True
-    session["id"] = user.get("id")
-    session["email"] = user.get("email")
-    session["full_name"] = user.get("full_name")
-
-    return True
-
-    # return username == config.username and password == config.password
-
+    stored_password = user.get("password").encode('utf-8')
+    # Verify the password with the stored hash
+    if bcrypt.checkpw(password.encode('utf-8'), stored_password):
+        session["is_logged_in"] = True
+        session["id"] = user.get("id")
+        session["email"] = user.get("email")
+        session["full_name"] = user.get("full_name")
+        return True
+    else:
+        return False
 
 def authenticate():
     """ Sends a 401 response that enables basic auth. """
     return Response('Not authorized to access this URL.\n'
                     'You have to login with proper credentials', 401,
                     {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
 
 def requires_auth(func):
     @wraps(func)
